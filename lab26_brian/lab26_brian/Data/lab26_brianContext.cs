cc118150cbcda099ecff7b7b3f964ffc6536b892
Brian McFarlane <bmcfarlane82@gmail.com>
2017-10-17 10:27:08 -0700

    Added model and scaffoldedededed

---
+++ lab26_brian/lab26_brian/Data/lab26_brianContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab26_brian.Models
+{
+    public class lab26_brianContext : DbContext
+    {
+        public lab26_brianContext (DbContextOptions<lab26_brianContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<lab26_brian.Models.CMS> CMS { get; set; }
+    }
+}
---
+++ lab26_brian/lab26_brian/Models/CMS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab26_brian.Models
+{
+    public class CMS
+    {
+        [Key]
+        public int CustomerID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        [DisplayName("Image URL")]
+        public string ImageURL { get; set; }
+        public bool IsPublished { get; set; }
+    }
+}
