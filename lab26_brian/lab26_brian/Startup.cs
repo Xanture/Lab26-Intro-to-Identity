1a74335e18972442b181ffca40cf3c16fd11705e
Brian McFarlane <bmcfarlane82@gmail.com>
2017-10-17 11:09:06 -0700

    Added UseAuthentication to configure. Toubleshooting bug when adding migration

--- lab26_brian/lab26_brian/Startup.cs
+++ lab26_brian/lab26_brian/Startup.cs
@@ -38,10 +38,13 @@ namespace lab26_brian
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
